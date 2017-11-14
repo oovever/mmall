341f0f4a9bce07085edfabe452356a8ae0c6b5a4
Codemupf <mupengfei6688@163.com>
2017-11-14 15:40:55 +0800

    登录状态下的更新密码操作

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -24,4 +24,5 @@ public interface UserMapper {
     String selectQuestionByUsername(String username);
     int checkAnswer(@Param("username")String username,@Param("question")String question,@Param("answer")String answer);
     int updatePasswordByUsername(@Param("username")String username,@Param("passwordNew")String passwordNew);
+    int checkPassword(@Param(value="password")String password,@Param("userId")Integer userId);
 }
--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -14,5 +14,6 @@ public interface IUserService {
     ServerResponse selectQuestion(String username);
     ServerResponse<String> checkAnswer(String username,String question,String answer);
     ServerResponse<String> forgetResetPassword(String username,String passwordNew,String forgetToken);
+    ServerResponse<String> resetPassword(String passwordOld,String passwordNew,User user);
 
 }
