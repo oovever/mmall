eb0e70282a7ea8e5555db1fa7269c62e8b7b1724
Codemupf <mupengfei6688@163.com>
2017-11-14 22:08:20 +0800

    平级分类查询功能

--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -1,6 +1,9 @@
 package com.mmall.service;
 
 import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
 
 /**
  * Created by OovEver on 2017/11/14.
@@ -9,4 +12,6 @@ public interface ICategoryService {
     public ServerResponse addCategory(String categoryName, Integer parentId);
 
     public ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+
+    public ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
 }
--- src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
@@ -5,14 +5,21 @@ import com.mmall.dao.CategoryMapper;
 import com.mmall.pojo.Category;
 import com.mmall.service.ICategoryService;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by OovEver on 2017/11/14.
  */
 @Service("iCategoryService")
 public class CategoryServiceImpl implements ICategoryService {
+    private Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
     @Autowired
     private CategoryMapper categoryMapper;
     @Override
@@ -44,5 +51,14 @@ public class CategoryServiceImpl implements ICategoryService {
             return ServerResponse.createBySuccess("更新品类名字成功");
         }
         return ServerResponse.createByErrorMessage("更新品类名字失败");
+    }
+
+    @Override
+    public ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId) {
+        List<Category> categoryList = categoryMapper.selectCategoryChildrenByParentId(categoryId);
+        if (CollectionUtils.isEmpty(categoryList)) {
+            logger.info("未找到当前分类的子分类");
+        }
+        return ServerResponse.createBySuccess(categoryList);
     }
 }
