09a9001da123c2064167764a860db837ed2cd060
Codemupf <mupengfei6688@163.com>
2017-11-16 17:01:06 +0800

    完成购物车增加功能，同时完成bigdecimal计算价格的工具的开发

---
+++ src/main/java/com/mmall/service/ICartService.java
@@ -0,0 +1,11 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.vo.CartVo;
+
+/**
+ * Created by OovEver on 2017/11/16.
+ */
+public interface ICartService {
+    ServerResponse<CartVo> add(Integer userId, Integer productId, Integer count);
+}
